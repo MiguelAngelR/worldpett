2d3d99d9c1c4f742379804e3de5ad34f95e20a61
MiguelAngelR <miguel.rios.alonso@outlook.com>
2018-06-08 14:08:47 -0500

    Agregue archivos

---
+++ src/worldpett/FXMLDocumentController.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldpett;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Michael Liebheart
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private TextField etUsuario;
+    @FXML
+    private PasswordField etPass;
+    @FXML
+    private Text tLogin;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+      
+    }
+    
+    @FXML
+    private void handleClose(ActionEvent event) {
+        System.exit(0);
+    }
+    
+    
+    @FXML
+    private void entraHome(ActionEvent event) throws IOException{
+      
+        String usuario, pass;
+        
+        usuario = etUsuario.getText();
+        pass = etPass.getText();
+        
+        if(usuario.equals("Miguel") && pass.equals("123456")){
+        
+            Parent home_page_parent = FXMLLoader.load(getClass().getResource("FXMLHome.fxml"));
+            Scene home_page_scene = new Scene(home_page_parent);
+            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            app_stage.setScene(home_page_scene);
+            app_stage.show();
+        } else {
+            tLogin.setVisible(true);
+        }
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
