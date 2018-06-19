b11efbddd6aaec1e1cf0698efa44ddcdd9f41d62
MiguelAngelR <miguel.rios.alonso@outlook.com>
2018-06-19 10:02:16 -0500

    Se agregaron ventanas de adminstrador y de capturistas , se le asigno ID a los elementos de la ventana de ID

--- src/worldpett/FXMLHomeVendedorController.java
+++ src/worldpett/FXMLHomeVendedorController.java
@@ -5,9 +5,19 @@
  */
 package worldpett;
 
+import base_datos.AlertBox;
+import base_datos.Conectar;
+import base_datos.Producto;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,18 +26,50 @@ import javafx.fxml.Initializable;
  */
 public class FXMLHomeVendedorController implements Initializable {
 
+    private ObservableList <Producto> listaProductos;
+    
+    
+    @FXML TextField tfTotal;
+    @FXML TextField tfPago;
+    @FXML TextField tfCambio;
+    
+    int total,pago,cambio;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Conectar con;
+        
+        try {
+            con = new Conectar();
+            con.getConnection(); 
+            Producto.llenarTabla(con.getConnection(), listaProductos);
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLHomeVendedorController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        tfTotal.setText("100");
+        listaProductos = FXCollections.observableArrayList();
     }    
     
-    public void cerrarVentana(){
-        
-        System.exit(0);
-    }
+    public void cobrar(){
     
+       total = Integer.parseInt(tfTotal.getText());
+       pago = Integer.parseInt(tfPago.getText());
+       
+       if(pago < total){
+           
+           AlertBox.pantalla("Error al cobrar", "La cantidad de efectivo ingresada es menor \n por favor ingrese una cantidad superior al total");
+           
+       }else{
+   
+           cambio = pago - total;
+           tfCambio.setText(Integer.toString(cambio));
+       }
+       
+    }
     
 }
