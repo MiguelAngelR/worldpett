7513febaf15ec666048ca3ff92024a63acc03ec1
Alan Sánchez <sanchezpienda03@gmail.com>
2018-06-21 20:36:30 -0500

    Creadas clases Proveedor y ProveedorDAO

---
+++ src/base_datos/ProveedorDAO.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base_datos;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import valueObject.Proveedor;
+
+/**
+ *
+ * @author Michael Liebheart
+ */
+public class ProveedorDAO extends BaseDataAccessObject {
+
+    
+    public ProveedorDAO() throws SQLException {
+        super();
+    }
+    
+    public void insertarProveedor(String nombre, String rfc) throws SQLException {
+        PreparedStatement prepareProveedor = connection.prepareStatement("INSERT INTO proveedores(nombre, rfc) VALUES (?,?)");
+        prepareProveedor.setString(1,nombre);
+        prepareProveedor.setString(2,rfc);
+        prepareProveedor.executeUpdate();
+        
+        prepareProveedor.close();
+    }
+    
+    public void modificarProveedor(int id, String nombre, String rfc) throws SQLException {
+        PreparedStatement prepareProveedor = connection.prepareStatement("UPDATE proveedores SET nombre = ?, rfc = ? WHERE id = ?");
+        prepareProveedor.setString(1,nombre);
+        prepareProveedor.setString(2,rfc);
+        prepareProveedor.setInt(3,id);
+        prepareProveedor.executeUpdate();
+        
+        prepareProveedor.close();
+    }
+    
+    public ArrayList<Proveedor> getProveedores() throws SQLException {
+        ArrayList<Proveedor> proveedores = new ArrayList<Proveedor>();
+        ResultSet rs = null;
+        
+        PreparedStatement prepareEmpleado = connection.prepareStatement("SELECT id, nombre, rfc from empleados");
+        rs = prepareEmpleado.executeQuery();
+        
+        while(rs.next()) {
+            Proveedor proveedor = new Proveedor();
+            
+            proveedor.setNombre(rs.getString("nombre"));
+            proveedor.setRFC(rs.getString("rfc"));
+            proveedor.setId(rs.getInt("id"));
+            
+            proveedores.add(proveedor);
+        }
+        
+        prepareEmpleado.close();
+        
+        return proveedores;
+    }
+    
+    public void deleteById(int id) throws SQLException {
+        PreparedStatement stmt = connection.prepareStatement("DELETE FROM proveedores where id = ?");
+        stmt.setInt(1, id);
+        stmt.executeUpdate();
+        stmt.close();
+    }
+    
+}
---
+++ src/valueObject/Proveedor.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package valueObject;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author Saul
+ */
+public class Proveedor {
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty nombre;
+    private SimpleStringProperty rfc;
+    
+    
+    public Proveedor() {
+        this.rfc = new SimpleStringProperty();
+        this.nombre = new SimpleStringProperty();
+        this.id = new SimpleIntegerProperty();
+    }
+    
+    public Proveedor(String nombre, String rfc, int id) {
+        this.rfc = new SimpleStringProperty(rfc);
+        this.nombre = new SimpleStringProperty(nombre);
+        this.id = new SimpleIntegerProperty(id);
+    }
+    
+    public int getId() {
+        return id.get();
+    }
+    
+    public void setId(int id) {
+        this.id.set(id);
+    }
+    
+    public String getNombre() {
+        return nombre.get();
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre.set(nombre);
+    }
+    
+    public String getRFC() {
+        return rfc.get();
+    }
+
+    public void setRFC(String rfc) {
+        this.rfc.set(rfc);
+    }
+}
